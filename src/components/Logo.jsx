67b00cc189d20a4240c6595f0d1d32799eddad85
MatyAlfonso <matt1998boca@gmail.com>
2022-08-02 15:22:25 +0200

    Add components and pages folder. Create HomePage and NavBar related components

---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+export const Logo = () => {
+    return (
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/800px-Rick_and_Morty.svg.png' alt='Rick and Morty logo' className='w-2/6 h-auto py-4' />
+    )
+}
