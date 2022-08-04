60f2e8f1eeb14ef147709dcf2e05a4c75ed1b308
Juangadis <juanestevecorena98@gmail.com>
2022-08-04 19:26:04 -0300

    Add styles

--- src/components/CharacterContainer.jsx
+++ src/components/CharacterContainer.jsx
@@ -1,8 +1,8 @@
 export const CharacterContainer = ({id, name, image}) => {
     return (
-        <div key={id}>
-            <img src={image} alt={name} />
-            <h1 className='my-4 text-center'>{name}</h1>
+        <div className="bg-[#2baeb8]/25 p-2 rounded-lg" key={id}>
+            <img src={image} alt={name} className='rounded-lg'/>
+            <h1 className='my-4 text-center text-white text-2xl'>{name}</h1>
         </div>
     )
 }
