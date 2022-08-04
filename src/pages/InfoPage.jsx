0259f436de84c6413434a0baf06f0d513b664611
Juangadis <juanestevecorena98@gmail.com>
2022-08-04 19:25:27 -0300

    Add styles

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -6,34 +6,33 @@ import { CharacterInfo } from '../components/CharacterInfo';
 export const InfoPage = () => {
 
   const [character, setCharacter] = useState([])
+  const [loading, setLoading] = useState([true])
 
   useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character/1')
+    fetch(`https://rickandmortyapi.com/api/character/1`)
       .then(response => {
         return response.json()
       })
       .then(character => {
         setCharacter(character)
+        setLoading(false)
       });
   },[])
 
   return (
     <>
       <ParticleBackground/>
-      <div className='py-12 
-                        px-8 
-                        flex 
-                        bg-gray-900 
-                        mb-10 
-                        rounded-lg 
-                        items-start 
-                        w-5/6 
-                        self-center 
-                        flex-wrap 
-                        justify-around'>
-        <CardImg image={character.image} />
-        <CharacterInfo character={character}/>
-      </div>
+      {loading 
+        ? 
+        <h1>Loading...</h1> 
+        :
+        <>
+          <div className='py-12 px-8 flex bg-[#2baeb8]/25 mb-10 rounded-lg items-start w-5/6 self-center flex-wrap justify-around'>
+          <CardImg image={character.image} />
+          <CharacterInfo character={character}/>
+          </div>
+        </>
+      }
     </>
   )
 }
