3a4ba8780fe880d97018e27d3e61453a18d1ce4d
MatyAlfonso <matt1998boca@gmail.com>
2022-08-04 13:48:30 +0200

    Add 404 page with styles and background with tsparticles

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import { PortalGif } from "../components";
+import { ParticleBackground } from "../components/ParticleBackground";
+
+
+export const NotFoundPage = () => {
+    return (
+        <>
+            <ParticleBackground />
+            <div className='flex flex-col'>
+                <div>
+                    <h1 className="text-center font-bold text-6xl text-white">Dimension C-404</h1>
+                    <PortalGif />
+                    <h2 className="text-center m-10 font-bold text-2xl text-white">“What are you here for? Just kidding, I don’t care.” — Rick</h2>
+                    <Link to="/">
+                        <button className='bg-[#2baeb8] text-2xl rounded-lg py-3 px-6 text-white block m-auto hover:bg-[#2699a1]'>Go back to home</button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
