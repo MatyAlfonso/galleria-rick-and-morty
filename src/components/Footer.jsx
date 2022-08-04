61c03c413c4d76e1fd539148f1a5de6c796fbbf2
MatyAlfonso <matt1998boca@gmail.com>
2022-08-04 13:50:55 +0200

    Fix some minor UI issues, footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ export const Footer = () => {
     return (
         <div className='p-14 mt-auto items-center text-center bg-[#2baeb8] text-white font-bold'>
             <p>&copy; Copyright 2022 - Rick and Morty Galleria</p>
-            <p>Todos los derechos reservados</p>
+            <a href='https://github.com/MatyAlfonso/'><p className='hover:text-black'>Matías Alfonso</p></a>
+            <a href='https://github.com/Juangadis/'><p className='hover:text-black'>Juan Pablo Estevecorena</p></a>
         </div>
     )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export * from './Footer';
 export * from './Logo';
 export * from './NavBar';
+export * from './PortalGif';
 export * from './SearchBar';
